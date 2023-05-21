d9a5bab38d01ad2d6a430a1a4eafc7b1363d88c5
BagOfLays <108637656+bagoflays@users.noreply.github.com>
2023-05-21 16:14:57 -0700

    Add files via upload

---
+++ pygoofball.py
@@ -0,0 +1,16 @@
+import time
+import webbrowser
+def install(NO):
+    if NO == 'butterfly':
+        print('Installing butterfly2...')
+        webbrowser.open('https://drive.google.com/uc?export=download&id=1NHOvUJ6pAEQYtX9ONmUWctrcySMIuM-9')
+        time.sleep(6)
+    if NO == 'fishy':
+        print('making a fish fillet...')
+        webbrowser.open('https://drive.google.com/uc?export=download&id=1CO-RCdYev8nXCHcdTiQklrvlgmq5kPhH')
+        time.sleep(6)
+
+
+def tutorial():
+    print('How to use PyGoofball txt is in the files')
+    time.sleep(60)
